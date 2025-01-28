5e76018ef93fdedcd6951d69084a135e8dd13f28
Samuel Mongare <samuelmongare2002@gmail.com>
2025-01-28 23:16:02 +0000

    Notion library imported and client created

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from config import NOTION_PAGE_ID
+from config import NOTION_PAGE_ID, NOTION_KEY
+import os
+from notion_client import Client
 
 def main():
     print('Artemis has access to Notion page:', NOTION_PAGE_ID)
+    notion = Client(auth=NOTION_KEY)
+    list_users_response = notion.users.list()
+    print(list_users_response)
 
 if __name__ == '__main__':
     main()
