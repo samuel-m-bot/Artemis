e92de0c768c4c56c365d00a9b9dded937c240502
Samuel Mongare <samuelmongare2002@gmail.com>
2025-03-11 21:28:46 +0000

    Added query of Habits database

--- config/settings.py
+++ config/settings.py
@@ -5,5 +5,6 @@ load_dotenv()
 
 NOTION_KEY = os.getenv('NOTION_KEY')
 NOTION_PAGE_ID = os.getenv('NOTION_PAGE_ID')
+NOTION_DATABASE_ID = os.getenv('NOTION_DATABASE_ID')
 
 DEBUG_MODE = os.getenv('DEBUG_MODE', 'False') 
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from config import NOTION_PAGE_ID, NOTION_KEY
-import os
+from config import NOTION_PAGE_ID, NOTION_KEY, NOTION_DATABASE_ID
+import json
 from notion_client import Client
 
 notion = Client(auth=NOTION_KEY)
@@ -15,18 +15,37 @@ def find_database(page_id):
 
 def main():
     print('Artemis has access to Notion page:', NOTION_PAGE_ID)
-    list_users_response = notion.users.list()
-    print(list_users_response)
-    print(notion.pages.retrieve(NOTION_PAGE_ID))
 
-    database_id = find_database(NOTION_PAGE_ID)
+    response = notion.databases.query(database_id=NOTION_DATABASE_ID)
 
-    if database_id:
-        print(f"Found Database ID: {database_id}")
-        tasks = notion.databases.query(database_id)
-        print(tasks)  
-    else:
-        print("No database found in this page.")
+    days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+    tasks = []
+    for item in response["results"]:
+        task = {
+            "id": item["id"],
+            "name": (
+                item["properties"]["Name"]["title"][0]["text"]["content"]
+                if "Name" in item["properties"] and item["properties"]["Name"]["title"]
+                else "Untitled"
+            ),
+            "created_time": item["created_time"],
+            "last_edited_time": item["last_edited_time"]
+        }
+
+        #There is a checkbox for each day (default to False if missing)
+        for day in days_of_week:
+            task[day] = (
+                item["properties"][day]["checkbox"]
+                if day in item["properties"] and "checkbox" in item["properties"][day]
+                else False
+            )
+
+        tasks.append(task)
+
+    json_output = json.dumps(tasks, indent=4)
+
+    print(json_output)
 
 if __name__ == '__main__':
     main()
